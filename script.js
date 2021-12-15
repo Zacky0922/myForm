180209eff590923800d4cb7af4d120c4ef2c23df
Zacky <abcn00200@yahoo.co.jp>
2021-12-15 22:57:39 +0900

    fix
    とりあえず使用には耐える

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
-const MST = [
-  {
-    id: "A1",
-    vendor: "メーカーA",
-    lot: "A1",
-    price: 100000,
-  },
-  {
-    id: "A2",
-    vendor: "メーカーA",
-    lot: "A2",
-    price: 110000,
-  },
-  {
-    id: "B1",
-    vendor: "メーカーB",
-    lot: "B1",
-    price: 200000,
-  },
-  {
-    id: "B2",
-    vendor: "メーカーB",
-    lot: "B2",
-    price: 210000,
-  },
-  {
-    id: "C1",
-    vendor: "メーカーC",
-    lot: "C1",
-    price: 300000,
-  },
-  {
-    id: "C2",
-    vendor: "メーカーC",
-    lot: "C2",
-    price: 310000,
-  },
-];
+// 商品リスト格納
+let MST;
 
 function init() {
   // 本日の日付を設定
@@ -47,7 +11,28 @@ function init() {
     ele[i].setAttribute("value", today);
   }
 
-  // メーカー初期設定
+  // 商品情報取得＆初期設定
+  fetch(
+    "https://script.google.com/macros/s/AKfycbwSkhbXG8i12d6wGKmRitG9xX2PDbJ5sY-GSX5p-uoFAnJ0Mqx2UeQpeTIenwdX79j6yQ/exec" +
+      "?id=1-DAni0Hnh_K94teeFW3OhSeD0b6jNeHObQvSvcp-e0A" +
+      "&name=商品情報"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      MST = json;
+      console.log(json);
+      // フォーム初期設定
+      initForm();
+      // 各種イベント設定
+      setEvents();
+    });
+}
+
+// フォーム初期設定
+function initForm() {
+  // メーカーリスト生成
   let vendors = [];
   for (let i in MST) {
     if (!vendors.includes(MST[i].vendor)) {
@@ -58,8 +43,8 @@ function init() {
       document.getElementById("merchandiseVendor").appendChild(opt);
     }
   }
-
-  // ;
+  // 初期型番リスト設定
+  changeVendor(MST[0].vendor);
 
   // URLクエリ取得
   let URLquery = location.search.replace("?", "");
@@ -69,25 +54,28 @@ function init() {
     let q = URLquery[i].split("=");
     query[q[0]] = q[1];
   }
+  // とりあえず初期設定
+  changeVendor(MST[0].vendor);
+  changeLot(MST[0].id);
   // 製品idがあればそれを設定
   if (query.id !== undefined) {
-    // alert(query.id);
     for (let i = 0; i < MST.length; i++) {
       if (query.id === MST[i].id) {
         // vendor設定
         document.getElementById("merchandiseVendor").value = MST[i].vendor;
         // 型番設定
-        setLot(MST[i].vendor,MST[i].lot);
+        changeVendor(MST[i].vendor, MST[i].lot);
         break;
       }
     }
+  } else {
+    // なければとりあえず初期メーカーリスト
+    changeVendor(document.getElementById("merchandiseVendor").value);
   }
-  // なければとりあえず初期メーカーリスト
-  else {
-    setLot(document.getElementById("merchandiseVendor").value);
-  }
+}
 
-  // 各種イベント設定
+// 各種イベント設定
+function setEvents() {
   // 郵便番号から住所取得
   document
     .getElementById("customerPostalcode")
@@ -105,57 +93,24 @@ function init() {
     .addEventListener("click", (e) => {
       copyCustomer2delivery();
     });
-  // 商品情報
+  // 商品情報：メーカー
   document
     .getElementById("merchandiseVendor")
     .addEventListener("change", (e) => {
-      setLot(e.target.value);
+      changeVendor(e.target.value);
     });
+  // 商品情報：型番
+  document.getElementById("merchandiseLot").addEventListener("change", (e) => {
+    changeLot(e.target.value);
+  });
   // 登録
   document.getElementById("regist").addEventListener("click", (e) => {
     regist();
   });
 }
 
-function changeVendor(e) {
-  console.log(e);
-}
-
-// 顧客情報→お届け先のコピー作成
-function copyCustomer2delivery() {
-  let ele0 = document
-    .getElementById("customerArea")
-    .getElementsByTagName("input");
-  let ele1 = document
-    .getElementById("deliveryArea")
-    .getElementsByTagName("input");
-  for (let i = 0; i < ele0.length; i++) {
-    ele1[i].value = ele0[i].value;
-  }
-}
-
-function setLot(val,defalt = false) {
-  document.getElementById("merchandiseLot").innerHTML = "";
-  let flag = false;
-  for (let i = 0; i < MST.length; i++) {
-    if (MST[i].vendor === val) {
-      let opt = document.createElement("option");
-      opt.value = MST[i].lot;
-      opt.innerHTML = MST[i].lot;
-      if (MST[i].lot === defalt) {
-        opt.selected = true;
-      }
-      document.getElementById("merchandiseLot").appendChild(opt);
-    }
-    if (flag) {
-      setPrice(MST[i].lot);
-    }
-  }
-}
-
-function setPrice(lot) {}
-
-function getAddress(e,area) {
+// 郵便番号から住所取得
+function getAddress(e, area) {
   let post = Number(e.target.value);
   // 桁数チェック
   if (post < 1000000 || 9999999 < post) {
@@ -172,13 +127,57 @@ function getAddress(e,area) {
       if (json.results === null) {
         return;
       }
-      document.getElementById(area+"Address1").value =
+      document.getElementById(area + "Address1").value =
         json.results[0].address1;
-      document.getElementById(area+"Address2").value =
+      document.getElementById(area + "Address2").value =
         json.results[0].address2;
-      document.getElementById(area+"Address3").value =
+      document.getElementById(area + "Address3").value =
         json.results[0].address3;
     });
+}
+
+// 顧客情報→お届け先のコピー作成
+function copyCustomer2delivery() {
+  let ele0 = document
+    .getElementById("customerArea")
+    .getElementsByTagName("input");
+  let ele1 = document
+    .getElementById("deliveryArea")
+    .getElementsByTagName("input");
+  for (let i = 0; i < ele0.length; i++) {
+    ele1[i].value = ele0[i].value;
+  }
+}
+
+function changeVendor(val, defalt = false) {
+  document.getElementById("merchandiseLot").innerHTML = "";
+  let flag = false;
+  for (let i = 0; i < MST.length; i++) {
+    if (MST[i].vendor === val) {
+      let opt = document.createElement("option");
+      opt.value = MST[i].id;
+      opt.innerHTML = MST[i].lot;
+      // リスト先頭をとりあえず価格設定
+      if (flag) {
+        document.getElementById("merchandiseVendorPrice").value = MST[i].price;
+        flag = true;
+      }
+      if (MST[i].lot === defalt) {
+        opt.selected = true;
+        document.getElementById("merchandiseVendorPrice").value = MST[i].price;
+      }
+      document.getElementById("merchandiseLot").appendChild(opt);
+    }
+  }
+}
+
+function changeLot(id) {
+  for (let i = 0; i < MST.length; i++) {
+    if (MST[i].id == id) {
+      document.getElementById("merchandiseVendorPrice").value = MST[i].price;
+      return;
+    }
+  }
 }
 
 // スプシ書き込み
@@ -187,24 +186,63 @@ function regist() {
     "https://script.google.com/macros/s/AKfycbwauCquwHoxmbnmHa7dkvLvSRDKHYylHD5EllOVW25f7YO3z-qPkoRJ999cN9xUx1Vm/exec" +
     "?id=1-DAni0Hnh_K94teeFW3OhSeD0b6jNeHObQvSvcp-e0A" +
     "&name=販売情報";
-  let areaIds = ["customerArea", "deliveryArea", "merchandise", "manager"];
+  let areaIds = [
+    "customerName",
+    "customerFurigana",
+    "customerMob",
+    "customerTel",
+    "customerPostalcode",
+    "customerAddress1",
+    "customerAddress2",
+    "customerAddress3",
+    "customerAddress4",
+    "deliveryName",
+    "deliveryFurigana",
+    "deliveryMob",
+    "deliveryTel",
+    "deliveryPostalcode",
+    "deliveryAddress1",
+    "deliveryAddress2",
+    "deliveryAddress3",
+    "deliveryAddress4",
+    "merchandiseVendor",
+    "merchandiseLot",
+    "merchandiseVendorPrice",
+    "merchandiseQuantity",
+    "merchandiseSellingPrice",
+    "merchandiseSpecialPrice",
+    "merchandiseDate",
+    "merchandiseFare",
+    "merchandiseOtherCost",
+    "managerStaff",
+    "managerSales",
+    "manegerText",
+  ];
   let query = "";
   let q = 1;
   for (let i = 0; i < areaIds.length; i++) {
-    if (i == 2) {
-      query +=
-        "&col" + q + "=" + document.getElementById("merchandiseVendor").value;
-      q++;
-      query +=
-        "&col" + q + "=" + document.getElementById("merchandiseLot").value;
-      q++;
-    }
-    let ele = document.getElementById(areaIds[i]).getElementsByTagName("input");
-    for (let j = 0; j < ele.length; j++) {
-      query += "&col" + q + "=" + ele[j].value;
-      q++;
+    switch (areaIds[i]) {
+      default:
+        // console.log(document.getElementById(areaIds[i]).value);
+        query += `&col${i + 1}=${encodeURI(
+          String(document.getElementById(areaIds[i]).value)
+        )}`;
     }
   }
-  fetch(encodeURI(GAS + query));
+  fetch(GAS + query)
+    .then(res => {
+      console.log(res)
+    });
+  document.getElementById("debug").innerText = query;
   alert("登録完了");
+}
+
+// 禁則処理
+function errorText(val) {
+  let errorLetters = ["#", "&", "="];
+  for (let i = 0; i < errorLetters.length; i++) {
+    if (val.indexOf(errorLetters[i]) > 0) {
+      alert(`半角「${errorLetters[i]}」は使用できません`);
+    }
+  }
 }
